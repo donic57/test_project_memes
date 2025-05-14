42533191f1a52bcd9eb3c6e22065abcc54bebc70
donic57 <lyadov_l@list.ru>
2025-05-14 16:34:40 +0300

    project_new

---
+++ tests/coftest.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+
+
+@pytest.fixture(scope='session')
+def authorize():
+    body = {
+        "name": "lyadov"
+    }
+    response = requests.post(
+        'http://167.172.172.115:52355/authorize', json=body).json()
+    return {'Authorization': response['token']}
